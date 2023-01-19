d227bfa66d4a1ce2f5f18876d06b75f22e670d80
byetevinn <stevan14padilha@gmail.com>
2023-01-19 16:50:13 -0300

    Fix: Input value for BRL mask

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -1,31 +1,38 @@
-import { useContext, useEffect, useState } from "react";
+import { ChangeEventHandler, useContext, useEffect, useState } from "react";
 import { ReceiveContext } from "../../contexts/ReceiveContext";
 import { StyledDiv } from "./style";
-import { useForm } from "react-hook-form";
 import { IReciveProps } from "./interfaces";
 
 function Form() {
-  const { register, handleSubmit } = useForm();
   const [inputValue, setInputValue] = useState({} as IReciveProps);
-  const [saleValue, setSaleValue] = useState(String);
+  const [saleValue, setSaleValue] = useState(String || Number);
 
   const { getReceive } = useContext(ReceiveContext);
 
-  const onSubmit = (data: any) => {
-    setInputValue({ ...data });
+  const onSubmit: ChangeEventHandler<HTMLInputElement> = (data) => {
+    const name = data.target.name;
+    const value = data.target.value;
+
+    if (name === "installments") {
+      setInputValue({ ...inputValue, installments: value });
+    } else {
+      setInputValue({ ...inputValue, mdr: value });
+    }
   };
 
-  const onSale = (data: any) => {
-    valueBRL(parseInt(data.target.value));
+  const onSale: ChangeEventHandler<HTMLInputElement> = (data) => {
+    let value = stringToNumber(data.target.value);
+
+    const amount = value.toString();
+
+    setInputValue({ ...inputValue, amount });
+
+    valueBRL(value);
   };
 
-  useEffect(() => {
-    const count = setTimeout(() => {
-      getReceive(inputValue);
-    }, 600);
-
-    return () => clearTimeout(count);
-  }, [inputValue]);
+  const stringToNumber = (value: String) => {
+    return parseInt(value.replace(/[^0-9]/g, "")) / 100;
+  };
 
   const valueBRL = (value: Number) => {
     const newValue = value.toLocaleString("pt-BR", {
@@ -34,35 +41,43 @@ function Form() {
     });
 
     setSaleValue(newValue);
-
-    const input: any = document.getElementById("amount");
   };
 
-  /* console.log(saleValue); */
+  useEffect(() => {
+    const count = setTimeout(() => {
+      const objValues = Object.values(inputValue);
+
+      if (objValues.length >= 3) {
+        if (objValues.every((value) => value !== "")) {
+          getReceive(inputValue);
+        }
+      }
+    }, 600);
+
+    return () => clearTimeout(count);
+  }, [inputValue]);
 
   return (
     <StyledDiv>
       <h1>Simule sua Antecipação</h1>
-      <form onChange={handleSubmit(onSubmit)}>
+      <form>
         <ul>
           <li>
             <p>Informe o valor da venda</p>
             <input
-              type="number"
-              {...register("amount")}
-              placeholder="R$ 0,00"
               onChange={onSale}
-              id="amount"
+              defaultValue={"R$ 0,00"}
+              value={saleValue}
             />
           </li>
           <li>
             <p>Em quantas parcelas</p>
-            <input type="number" {...register("installments")} />
+            <input type="number" onChange={onSubmit} name="installments" />
             <span>Máximo de 12 parcelas</span>
           </li>
           <li>
             <p>Informe o percentual de MDR</p>
-            <input type="number" {...register("mdr")} />
+            <input type="number" onChange={onSubmit} name={"mdr"} />
           </li>
         </ul>
       </form>
